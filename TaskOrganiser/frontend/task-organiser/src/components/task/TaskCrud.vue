c0cd694316e3771e79a4dd62d1bf9d9300d4af9e
Cong Nguyen <congnt87@gmail.com>
2025-02-23 11:26:15 +0700

    Add form to TaskCrud component

---
+++ TaskOrganiser/frontend/task-organiser/src/components/task/TaskCrud.vue
@@ -0,0 +1,70 @@
+<script setup>
+import {ref} from 'vue';
+
+const dropdownItems = ref([
+    {name: 'Option 1', code: 'Option 1'},
+    {name: 'Option 2', code: 'Option 2'},
+    {name: 'Option 3', code: 'Option 3'}
+]);
+
+const dropdownItem = ref(null);
+</script>
+
+<template>
+    <Fluid>
+            <div class="w-full">
+                <Accordion value="0">
+                    <AccordionPanel value="0">
+                        <AccordionHeader>Details</AccordionHeader>
+                        <AccordionContent>
+                            <div class=" flex flex-col gap-4 w-full">
+                                <div class="flex flex-col md:flex-row gap-4">
+                                    <div class="flex flex-wrap gap-2 w-full">
+                                        <label for="firstname2">Firstname</label>
+                                        <InputText id="firstname2" type="text"/>
+                                    </div>
+                                    <div class="flex flex-wrap gap-2 w-full">
+                                        <label for="lastname2">Lastname</label>
+                                        <InputText id="lastname2" type="text"/>
+                                    </div>
+                                </div>
+
+                                <div class="flex flex-wrap">
+                                    <label for="address">Address</label>
+                                    <Textarea id="address" rows="4"/>
+                                </div>
+
+                                <div class="flex flex-col md:flex-row gap-4">
+                                    <div class="flex flex-wrap gap-2 w-full">
+                                        <label for="state">State</label>
+                                        <Select id="state" v-model="dropdownItem" :options="dropdownItems"
+                                                optionLabel="name"
+                                                placeholder="Select One" class="w-full"></Select>
+                                    </div>
+                                    <div class="flex flex-wrap gap-2 w-full">
+                                        <label for="zip">Zip</label>
+                                        <InputText id="zip" type="text"/>
+                                    </div>
+                                </div>
+                            </div>
+
+                        </AccordionContent>
+                    </AccordionPanel>
+                    <AccordionPanel value="1">
+                        <AccordionHeader>Header II</AccordionHeader>
+                        <AccordionContent>
+                            <p class="m-0">
+                                Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium
+                                doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore
+                                veritatis et quasi architecto beatae vitae dicta sunt
+                                explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut
+                                fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi
+                                nesciunt. Consectetur, adipisci velit, sed quia non
+                                numquam eius modi.
+                            </p>
+                        </AccordionContent>
+                    </AccordionPanel>
+                </Accordion>
+        </div>
+    </Fluid>
+</template>
--- TaskOrganiser/frontend/task-organiser/src/views/Dashboard.vue
+++ TaskOrganiser/frontend/task-organiser/src/views/Dashboard.vue
@@ -2,6 +2,8 @@
 import {CustomerService} from '@/service/CustomerService';
 import {ProductService} from '@/service/ProductService';
 import {FilterMatchMode, FilterOperator} from '@primevue/core/api';
+import TaskCrud from '@/components/task/TaskCrud.vue';
+
 import {onBeforeMount, reactive, ref} from 'vue';
 
 const customers1 = ref(null);
@@ -94,7 +96,6 @@ function formatCurrency(value) {
 </script>
 
 <template>
-
     <div class="card">
         <div class="font-semibold text-xl mb-4">Row Expansion</div>
         <DataTable v-model:expandedRows="expandedRows" :value="products" dataKey="id" tableStyle="min-width: 60rem">
@@ -129,96 +130,12 @@ function formatCurrency(value) {
                 </template>
             </Column>
             <template #expansion="slotProps">
-                <div class="p-4">
-                    <h5>Orders for {{ slotProps.data.name }}</h5>
-                    <DataTable :value="slotProps.data.orders">
-                        <Column field="id" header="Id" sortable></Column>
-                        <Column field="customer" header="Customer" sortable></Column>
-                        <Column field="date" header="Date" sortable></Column>
-                        <Column field="amount" header="Amount" sortable>
-                            <template #body="slotProps">
-                                {{ formatCurrency(slotProps.data.amount) }}
-                            </template>
-                        </Column>
-                        <Column field="status" header="Status" sortable>
-                            <template #body="slotProps">
-                                <Tag :value="slotProps.data.status.toLowerCase()"
-                                     :severity="getOrderSeverity(slotProps.data)"/>
-                            </template>
-                        </Column>
-                        <Column headerStyle="width:4rem">
-                            <template #body>
-                                <Button icon="pi pi-search"/>
-                            </template>
-                        </Column>
-                    </DataTable>
-                </div>
-                <div class="p-4">
-                    <h5>Dependencies for {{ slotProps.data.name }}</h5>
-                    <DataTable v-model:expandedRows="expandedRows" :value="products1" dataKey="id" tableStyle="min-width: 60rem">
-                        <template #header>
-                            <div class="flex flex-wrap justify-end gap-2">
-                                <Button text icon="pi pi-plus" label="Expand All" @click="expandAll"/>
-                                <Button text icon="pi pi-minus" label="Collapse All" @click="collapseAll"/>
-                            </div>
-                        </template>
-                        <Column expander style="width: 5rem"/>
-                        <Column field="name" header="Name"></Column>
-                        <Column header="Image">
-                            <template #body="slotProps">
-                                <img :src="`https://primefaces.org/cdn/primevue/images/product/${slotProps.data.image}`"
-                                     :alt="slotProps.data.image" class="shadow-lg" width="64"/>
-                            </template>
-                        </Column>
-                        <Column field="price" header="Price">
-                            <template #body="slotProps">
-                                {{ formatCurrency(slotProps.data.price) }}
-                            </template>
-                        </Column>
-                        <Column field="category" header="Category"></Column>
-                        <Column field="rating" header="Reviews">
-                            <template #body="slotProps">
-                                <Rating :modelValue="slotProps.data.rating" readonly/>
-                            </template>
-                        </Column>
-                        <Column header="Status">
-                            <template #body="slotProps">
-                                <Tag :value="slotProps.data.inventoryStatus" :severity="getStockSeverity(slotProps.data)"/>
-                            </template>
-                        </Column>
-                        <template #expansion="slotProps">
-                            <div class="p-4">
-                                <h5>Orders for {{ slotProps.data.name }}</h5>
-                                <DataTable :value="slotProps.data.orders">
-                                    <Column field="id" header="Id" sortable></Column>
-                                    <Column field="customer" header="Customer" sortable></Column>
-                                    <Column field="date" header="Date" sortable></Column>
-                                    <Column field="amount" header="Amount" sortable>
-                                        <template #body="slotProps">
-                                            {{ formatCurrency(slotProps.data.amount) }}
-                                        </template>
-                                    </Column>
-                                    <Column field="status" header="Status" sortable>
-                                        <template #body="slotProps">
-                                            <Tag :value="slotProps.data.status.toLowerCase()"
-                                                 :severity="getOrderSeverity(slotProps.data)"/>
-                                        </template>
-                                    </Column>
-                                    <Column headerStyle="width:4rem">
-                                        <template #body>
-                                            <Button icon="pi pi-search"/>
-                                        </template>
-                                    </Column>
-                                </DataTable>
-                            </div>
-                        </template>
-                    </DataTable>
+                <div class="p-1">
+                    <TaskCrud></TaskCrud>
                 </div>
             </template>
         </DataTable>
     </div>
-
-
 </template>
 
 <style scoped lang="scss">
