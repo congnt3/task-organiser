18783a7295e164a17f95dad9e44b0b0ef72e5fe5
Cong Nguyen <congnt87@gmail.com>
2025-03-03 23:24:17 +0700

    create parameterized task crud form

--- TaskOrganiser/frontend/task-organiser/src/components/task/TaskCrud.vue
+++ TaskOrganiser/frontend/task-organiser/src/components/task/TaskCrud.vue
@@ -11,9 +11,18 @@ interface Props {
     deadline?: string;
 }
 
-// Define the props with defaults
-const model = withDefaults(defineProps<Props>(), {
+// // Define the props with defaults
+const props = withDefaults(defineProps<Props>(), {
     parentTask: "/"
+})
+
+let model = ref({
+    parentTask: props.parentTask,
+    code: props.code,
+    name: props.name,
+    description: props.description,
+    status: props.status,
+    deadline: props.deadline
 })
 
 const dropdownStates = ref([
@@ -21,49 +30,44 @@ const dropdownStates = ref([
     {name: 'In Progress', code: 'In Progress'},
     {name: 'Completed', code: 'Completed'}
 ]);
-
-const dropdownState = ref(null);
-
 </script>
 
 <template>
     <Fluid>
         <div class="w-full">
             <div class=" flex flex-col gap-4 w-full">
-                <div class="flex flex-col lg:flex-row gap-4">
+                <div class="flex flex-col md:flex-row gap-4">
                     <div class="flex flex-wrap gap-2 w-full">
                         <label for="parent">Parent</label>
-                        <InputText id="parent" type="text" :value="model.parentTask"/>
-
+                        <InputText id="parent" type="text" :value="props.parentTask"/>
                     </div>
                     <div class="flex flex-wrap gap-2 w-full">
                         <label for="taskid">Task ID</label>
                         <InputText id="taskid" type="text"/>
                     </div>
                 </div>
-                <div class="flex flex-col lg:flex-row gap-4">
+                <div class="flex flex-col md:flex-row gap-4">
                     <div class="flex flex-wrap gap-2 w-full">
                         <label for="name">Name</label>
-                        <InputText id="name" type="text"/>
+                        <InputText id="name" type="text" v-model="model.name"/>
                     </div>
                 </div>
 
                 <div class="flex flex-wrap">
                     <label for="description">Description</label>
-                    <Textarea id="description" rows="4"/>
+                    <Textarea id="description" rows="4" v-model="model.description"/>
                 </div>
-
                 <div class="flex flex-col md:flex-row gap-4">
                     <div class="flex flex-wrap gap-2 w-full">
                         <label for="status">Status</label>
-                        <Select id="status" v-model="dropdownState" :options="dropdownStates"
+                        <Select id="status" v-model="model.status" :options="dropdownStates"
                                 optionLabel="name"
                                 placeholder="Select One" class="w-full"
                                 default-value="New"></Select>
                     </div>
                     <div class="flex flex-wrap gap-2 w-full">
                         <label for="deadline">Deadline</label>
-                        <InputText id="deadline" type="text"/>
+                        <InputText id="deadline" type="text" v-model="model.deadline"/>
                     </div>
                 </div>
             </div>
